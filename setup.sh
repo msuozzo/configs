7cc187d2e2ec768b5f8a0077b9fcc8018833855f
Matthew Suozzo <matthew.suozzo@gmail.com>
2026-04-10 14:01:46 -0400

    Use short ln flag

--- setup.sh
+++ setup.sh
@@ -3,21 +3,21 @@
 ## RC
 config_dir=$(dirname "$(readlink -f "$0")")
 for f in rc/bashrc rc/vimrc rc/ignore git/gitconfig git/gitignore; do
-  ln -f --symbolic \
+  ln -f -s \
     "${config_dir}/${f}" \
     "${HOME}/.$(basename "${f}")"
 done
 mkdir -p "${HOME}/.config/jj"
-ln -f --symbolic "${config_dir}/jj/config.toml" "${HOME}/.config/jj/config.toml"
+ln -f -s "${config_dir}/jj/config.toml" "${HOME}/.config/jj/config.toml"
 for p in ${config_dir}/jj/confd_*.toml; do
   mkdir -p "${HOME}/.config/jj/conf.d/"
   fname=$(basename "$p")
-  ln -f --symbolic \
+  ln -f -s \
     "$p" \
     "${HOME}/.config/jj/conf.d/${fname#confd_}"
 done
 mkdir -p "${HOME}/.vim/colors/"
-ln -f --symbolic \
+ln -f -s \
   "${config_dir}/kanagawa-dragon.vim" \
   "${HOME}/.vim/colors/kanagawa-dragon.vim"
 
